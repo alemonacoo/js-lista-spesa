56e68b5c7e12fbd6bccb737e3cc76eecb4db11fe
alemonacoo <ale.monaco@outlook.com>
2022-08-30 14:22:24 +0200

    exercise finished!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("JS OK!");
+
+const shoppingArray = ['pane', 'latte', 'biscotti', 'zucchero', 'caffè', 'coca-cola'];
+const shoppingList = document.getElementById("shopping-list");
+let itemsCounter = 0;
+
+
+while(itemsCounter < shoppingArray.length){
+    console.log(itemsCounter);
+    console.log(shoppingArray[itemsCounter]);
+    shoppingList.innerHTML += `<li>${shoppingArray[itemsCounter]}</li>`;
+    itemsCounter++;
+}
